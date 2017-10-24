2d0df518f49583f985bc3da53ec7a63c24534e5e
bangbang93 <bangbang93@163.com>
2017-10-24 12:37:02 +0800

    修正有多个图片标签时只处理了第一个

--- index.js
+++ index.js
@@ -5,25 +5,31 @@
 module.exports = function (markdownIt, className = 'lozad') {
   const defaultImgRender = markdownIt.renderer.rules.image
   markdownIt.renderer.rules.image = function (tokens, idx, options, env, renderer) {
-    const token = tokens[0]
-    for(const attr of token.attrs) {
-      if (attr[0] === 'src') {
-        attr[0] = 'data-src'
-        break
+    for(const token of tokens) {
+      if (token.type !== 'image') continue
+      for(const attr of token.attrs) {
+        if (attr[0] === 'src') {
+          attr[0] = 'data-src'
+          break
+        }
       }
-    }
-    let hasClass = false
-    for(const attr of token.attrs) {
-      if (attr[0] === 'class') {
-        const classes = attr[1].split(' ')
-        classes.push(className)
-        attr[1] = classes.join(' ')
-        hasClass = true
-        break
+      let hasClass = false
+      for(const attr of token.attrs) {
+        if (attr[0] === 'class') {
+          if (attr[1].includes(className)) {
+            hasClass = true
+            break
+          }
+          const classes = attr[1].split(' ')
+          classes.push(className)
+          attr[1] = classes.join(' ')
+          hasClass = true
+          break
+        }
       }
-    }
-    if (!hasClass) {
-      token.attrs.push(['class', className])
+      if (!hasClass) {
+        token.attrs.push(['class', className])
+      }
     }
     return defaultImgRender.apply(this, arguments)
   }
--- test.js
+++ test.js
@@ -8,13 +8,16 @@ require('should')
 
 const md = new MarkdownIt().use(MarkdownItLozad)
 
-const testMd = '![aa](https://tva3.sinaimg.cn/crop.0.0.200.200.200/56e0fc78jw1e8qgp5bmzyj2050050aa8.jpg)'
+const testMd = `![aa](https://tva3.sinaimg.cn/crop.0.0.200.200.200/56e0fc78jw1e8qgp5bmzyj2050050aa8.jpg)
+![aa](https://tva3.sinaimg.cn/crop.0.0.200.200.200/56e0fc78jw1e8qgp5bmzyj2050050aa8.jpg)`
 
 describe('markdown-it-lozad', function () {
   it('should render data-src', function () {
     const html = md.render(testMd)
+    console.log(html)
     html.includes('data-src').should.true()
     html.includes('lozad').should.true()
+    html.match(/data-src/ig).length.should.eql(2)
   })
   it('should use custom class', function () {
     const md = new MarkdownIt().use(MarkdownItLozad, 'some-custom-lozad')
